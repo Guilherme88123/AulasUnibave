acbf7697930594e032012c4a4f5eeaa74c0b407c
Guilherme88123 <guilherme88123@gmail.com>
2026-04-07 21:15:03 -0300

    Add ProductService method implementation

--- Semestre01/LogicaProgramacao/Testes/Teste007/src/main/java/Service/Product/ProductService.java
+++ Semestre01/LogicaProgramacao/Testes/Teste007/src/main/java/Service/Product/ProductService.java
@@ -1,8 +1,13 @@
 package Service.Product;
 
 import Data.BaseRepository;
+import Data.HibernateHelper;
 import Domain.Interface.Product.IProductService;
 import Domain.Model.Product.ProductModel;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.hibernate.Session;
 
 import java.util.List;
 
@@ -10,7 +15,16 @@ public class ProductService
         extends BaseRepository<ProductModel>
         implements IProductService
 {
-    public List<ProductModel> GetByCategory(int categoryId) {
-        return List.of();
+    public List<ProductModel> GetByCategory(int categoryId)
+    {
+        try (Session session = HibernateHelper.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<ProductModel> cq = cb.createQuery(ProductModel.class);
+            Root<ProductModel> product = cq.from(ProductModel.class);
+
+            cq.select(product).where(cb.equal(product.get("categoryId"), categoryId));
+
+            return session.createQuery(cq).getResultList();
+        }
     }
 }
